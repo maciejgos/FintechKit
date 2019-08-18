75662b6a6462e45a2dc25ddfe91db66d30b48eb4
Maciej Gos <maciej@maciejgos.com>
2019-08-18 12:05:46 +0200

    Create resource dictionaries & merged dictionaries

---
+++ src/FintechKit/Styles/Theme.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FintechKit.Styles
+{
+    public partial class Theme : ResourceDictionary
+    {
+        public Theme()
+        {
+            InitializeComponent();
+        }
+    }
+}
